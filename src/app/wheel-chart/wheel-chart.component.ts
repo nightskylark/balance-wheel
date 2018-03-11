1f0b7eff7912baed9a378e16e2788771d121c1ab
nightskylark <eugen.zha@gmail.com>
2018-03-11 13:55:44 +0300

    Replace wheel categories labels by icons

--- src/app/wheel-chart/wheel-chart.component.ts
+++ src/app/wheel-chart/wheel-chart.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, SimpleChanges } from '@angular/core';
 
 @Component({
   selector: 'app-wheel-chart',
@@ -11,15 +11,15 @@ export class WheelChartComponent implements OnInit {
   @Input('categories') categories: object;
 
   wheelData;
+  showIcons = true;
+  chartElement;
+  icons = [];
 
   constructor() { }
 
-  ngOnChanges() {
-    var that = this;
-    if(that.categories) {
-      that.getCategory = function(argument) {
-        return that.categories[argument.value].name;
-      };
+  ngOnChanges(changes: SimpleChanges) {
+    if(changes.data || changes.categories) {
+      this.drawIcons();
     }
   }
 
@@ -41,15 +41,62 @@ export class WheelChartComponent implements OnInit {
     this.wheelData = mapData(this.data);
   }
 
-  getCategory(argument: any) :any {
-    return "";
-  }
+  onChartInit(e) {
+    this.chartElement = e.element;
 
-  // TODO: move to template after devextreme-angular update
-  onChartInit = function(e) {
+    // TODO: move to template after devextreme-angular update
     e.component.option({
       "barGroupPadding": 0
     });
-  };
+  }
+
+  onChartDrawn(e) {
+    this.drawIcons();
+  }
+
+  drawIcons() {
+    if(this.showIcons && this.categories && this.chartElement) {
+      this.icons.forEach(icon => icon.remove());
+
+      const ICON_SIZE_RATIO = 0.08;
+      const ICON_AREA_RATIO = 1.7;
+      const ICON_PADDING_RATIO = 0.25;
+
+      let wheelSize = this.chartElement.querySelector('.dxc-axes-group').getBBox();
+      let containerCenter = {
+        x: parseFloat(window.getComputedStyle(this.chartElement, null).getPropertyValue("width")) / 2,
+        y: parseFloat(window.getComputedStyle(this.chartElement, null).getPropertyValue("height")) / 2
+      }
+
+      let maxSize = wheelSize.width > wheelSize.height ? wheelSize.width : wheelSize.height;
+      let iconSize = maxSize * ICON_SIZE_RATIO;
+      let iconAreaRadius = iconSize * ICON_AREA_RATIO / 2;
+      let wheelRadius = maxSize / 2 + iconSize;
+
+      let iconsCount = this.wheelData.length;
+      let deltaAngle = 2 * Math.PI / iconsCount;
+      let startAngle = - Math.PI / 2 + deltaAngle / 2;
+
+      for(let i = 0; i < iconsCount; i++) { 
+        let angle = startAngle + i * deltaAngle;
+        let x = wheelRadius * Math.cos(angle) + containerCenter.x - iconAreaRadius;
+        let y = wheelRadius * Math.sin(angle) + containerCenter.y - iconAreaRadius;
+
+        let icon = document.createElement('div');
+        let category = this.categories[i + 1];
+
+        icon.className = 'dx-icon-' + category.icon + ' chart-icon';
+        icon.style.left = x + 'px';
+        icon.style.top = y + 'px';
+        icon.style.color = category.color;
+        icon.style['border-color'] = category.color;
+        icon.style['font-size'] = Math.round(iconSize) + 'px';
+        icon.style.padding = Math.round(iconSize / 4) + 'px';
+
+        this.chartElement.appendChild(icon);
+        this.icons.push(icon);
+      }
+    }
+  }
 
 }
